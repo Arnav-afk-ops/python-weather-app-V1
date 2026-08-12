2f500095f86822d53eab23984fe8227c24ec0a04
Arnav <khairmodearnav@gmail.com>
2026-08-12 20:22:53 +0530

    Refactor weather.py to use environment variables

--- weather.py
+++ weather.py
@@ -1,37 +1,40 @@
 import requests
-import json
-API_KEY = "ENTER YOUR API HERE"
-city = input("Enter a City : ")
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+city = input("Enter a City: ")
 
 url = "https://api.openweathermap.org/data/2.5/weather"
-request = {
-    "appid" : API_KEY,
-    "q": city,
-    "units":"metric"
 
+request = {
+    "appid": API_KEY,
+    "q": city,
+    "units": "metric"
 }
 
 
-def get_request() :
- response = requests.get(url,params = request)
- data = response.json()
- temp = data["main"]["temp"]
- humidity = data["main"]["humidity"]
- condition = data["weather"][0]["main"]
- description = data["weather"][0]["description"]
+def get_weather():
+    response = requests.get(url, params=request)
 
- print("===  WEATHER CLI  ===")
- print(f"Temp : {temp}°C")
- print(f"Humidity : {humidity}%")
- print(f"Condition : {condition}")
- print(f"Description : {description}")
+    if response.status_code == 200:
+        data = response.json()
+
+        temp = data["main"]["temp"]
+        humidity = data["main"]["humidity"]
+        condition = data["weather"][0]["main"]
+        description = data["weather"][0]["description"]
+
+        print("=== WEATHER CLI ===")
+        print(f"Temp : {temp}°C")
+        print(f"Humidity : {humidity}%")
+        print(f"Condition : {condition}")
+        print(f"Description : {description}")
+
+    else:
+        print("Couldn't retrieve weather data :(")
 
 
-if response.status_code == 200 :
- get_request()
-
-
-
-else :
- print("Couldn't retrieve weather data :(")
+get_weather()
